447e56eb2e51823bc81f967cb511324544c3974a
lou1s <lou1svuong.dev@gmail.com>
2025-07-04 07:52:38 +0700

    Enhance AnnouncementBar component styling and text emphasis
    - Changed background color to secondary for better visibility.
    - Updated text styles to use font-extrabold for the "TESTNET" label and adjusted font weight for improved emphasis.

--- components/announcement-bar/index.tsx
+++ components/announcement-bar/index.tsx
@@ -6,16 +6,16 @@ import { IconArrowRight } from "@tabler/icons-react";
 
 export default function AnnouncementBar() {
   return (
-    <div className="relative flex items-center justify-between bg-foreground mb-2 px-6 py-[0.60rem] rounded-xs lg:rounded-md text-[0.5rem] font-normal leading-5 text-primary-foreground lg:text-xs">
+    <div className="relative flex items-center justify-between bg-secondary mb-2 px-6 py-[0.60rem] rounded-xs lg:rounded-md text-[0.5rem] leading-5 text-secondary-foreground lg:text-xs font-semibold">
       <StartHighlight />
       <div className="absolute left-1/2 top-1/2 flex h-5 -translate-x-1/2 -translate-y-1/2 items-center gap-2 text-nowrap">
-        <p className="font-normal">
-          <span className="text-secondary">TESTNET</span> IS LIVE!
+        <p>
+          <span className="font-extrabold">TESTNET</span> IS LIVE!
         </p>
         <Separator className="h-4 w-px" orientation="vertical" />
         <Link
           href="/about-testnet"
-          className="flex items-center gap-2 text-nowrap text-primary-foreground"
+          className="flex items-center gap-2 text-nowrap"
         >
           Learn more about Testnets
           <IconArrowRight className="h-3 w-3" />
